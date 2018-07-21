220db82ca981441d8290a0f947f9c5ac5dfc7817
innowantai <innowantai@gmail.com>
2018-07-21 23:06:13 +0800

    Add mathRosePatterns

---
+++ 2_MathematicalRosePatterns/sketch.js
@@ -0,0 +1,35 @@
+ 
+
+let d = 8;
+let n = 5;
+let sliderD;
+let sliderN;
+function setup() {
+    createCanvas(400,400);
+    sliderD = createSlider(1,10,5);
+    sliderN = createSlider(1,10,5);
+}
+ 
+
+function draw() { 
+    d = sliderD.value();
+    n = sliderN.value();
+    let k = n / d  ;
+    background(51);
+    translate(width/2,height/2)
+
+    beginShape();
+    stroke(255);d
+    noFill();
+    strokeWeight(1);
+    for(let a = 0 ; a < TWO_PI * d ; a += 0.02){
+        let r = 200*cos(k*a);
+        let x = r * sin(a);
+        let y = r * cos(a);
+        vertex(x,y);
+    }
+
+    endShape(CLOSE);
+}
+
+ 
