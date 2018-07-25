920b6a93134f44a59ec0e9616a1fae3b22611a7a
innowantai <innowantai@gmail.com>
2018-07-25 09:10:27 +0800

    PerlinNoise Keep goning

--- 6_PerlinNoise/sketch.js
+++ 6_PerlinNoise/sketch.js
@@ -3,34 +3,60 @@
 // let xoff2 = 10000;
 // let yoff = 0;
 
-let inc = 0.01; 
-let scl = 20;
+/// start 19 mins 
+let inc = 0.1; 
+let scl = 10;
 let cols , rows ;
+let zoff = 0;
+
+let fr;
+
+let particles = [];
+
+
+
 function setup () {
-    createCanvas(500,500)
+    createCanvas(200,200)
     cols = floor(width / scl);
-    rows = floor(height / scl);
+    rows = floor(height / scl); 
+    fr = createP();
+
+    for(let i = 0 ; i < 100 ; i++){
+        particles[i] = new Particle()
+    }
 }
 
 
 function draw () { 
+    background(255);
     let yoff = 0;
     for (let y = 0 ; y < rows ; y ++){
         let xoff = 0;
         for (let x = 0 ; x < cols ; x ++){
             let index = (x + y * width) * 4;
-            let r = noise(xoff,yoff)*255;  
+            let angle = noise(xoff,yoff,zoff)*TWO_PI;  
+            let v = p5.Vector.fromAngle(angle);
             xoff += inc;
-
-            fill(random(255));
-            rect(x*scl,y*scl,scl,scl)
+            stroke(0,50);
+            push();
+            translate(x*scl,y*scl);
+            rotate(v.heading());
+            strokeWeight(1);
+            line(0,0,scl,0);
+            pop(0,0,scl,0);
+ 
         } 
         yoff += inc;
+        //zoff += 0.001;
     } 
-
-
-    noLoop();
  
+    for(let i = 0 ; i < particles.length ; i++){
+        particles[i].update();
+        particles[i].show();
+        particles[i].edges();
+    }
+
+    fr.html(floor(frameRate()))
       
 }
 
