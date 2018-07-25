fb3e27ebf28cb9427d07d1b72a949a6f6b433bdc
innowantai <innowantai@gmail.com>
2018-07-25 13:57:47 +0800

    PerlinNoise first Done

--- 6_PerlinNoise/sketch.js
+++ 6_PerlinNoise/sketch.js
@@ -12,51 +12,56 @@ let zoff = 0;
 let fr;
 
 let particles = [];
-
+let flowfield ;
 
 
 function setup () {
-    createCanvas(200,200)
+    createCanvas(400,300)
     cols = floor(width / scl);
     rows = floor(height / scl); 
     fr = createP();
 
-    for(let i = 0 ; i < 100 ; i++){
-        particles[i] = new Particle()
+    flowfield = new Array(cols * rows);
+
+    for(let i = 0 ; i < 3000 ; i++){
+        particles[i] = new Particle();
     }
 }
 
 
 function draw () { 
-    background(255);
+   // background(255);
     let yoff = 0;
     for (let y = 0 ; y < rows ; y ++){
         let xoff = 0;
         for (let x = 0 ; x < cols ; x ++){
-            let index = (x + y * width) * 4;
-            let angle = noise(xoff,yoff,zoff)*TWO_PI;  
-            let v = p5.Vector.fromAngle(angle);
+            let index = x + y * cols;
+            let angle = noise(xoff,yoff,zoff)*TWO_PI * 4;  
+            let v = p5.Vector.fromAngle(angle); 
+            v.setMag(1);
+            flowfield[index] = v; 
             xoff += inc;
             stroke(0,50);
-            push();
-            translate(x*scl,y*scl);
-            rotate(v.heading());
-            strokeWeight(1);
-            line(0,0,scl,0);
-            pop(0,0,scl,0);
+            // push();
+            // translate(x*scl,y*scl);
+            // rotate(v.heading());
+            // strokeWeight(1);
+            // line(0,0,scl,0);
+            // pop(0,0,scl,0);
  
         } 
         yoff += inc;
-        //zoff += 0.001;
+        zoff += 0.0003;
     } 
  
     for(let i = 0 ; i < particles.length ; i++){
+        particles[i].follow(flowfield);
         particles[i].update();
         particles[i].show();
         particles[i].edges();
     }
 
-    fr.html(floor(frameRate()))
+    //fr.html(floor(frameRate()))
       
 }
 
