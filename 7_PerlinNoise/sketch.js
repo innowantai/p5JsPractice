4a197a896df13858e565be7365052f6082fd6e58
innowantai <innowantai@gmail.com>
2018-07-25 17:00:50 +0800

    KeepPracitce

--- 7_PerlinNoise/sketch.js
+++ 7_PerlinNoise/sketch.js
@@ -1,29 +1,26 @@
 
 
-let inc = 0.05;
-let scl = 20;
-let xoff = 0;
+let xoff;
+let scl = 0.1;
 let xstart = 0;
+
 function setup () {
-    createCanvas(400,400)
+    createCanvas(800,400)
 }
 
 function draw () { 
     background(0);
-
-    beginShape();  
+    beginShape();
     noFill();
     xoff = xstart;
-    for(let x = 0 ; x < width; x++){
+    for(let x = 0 ; x < width ; x++){
         stroke(255);
-        //let y = map(noise(xoff),0,1,0,height);
-        y = map(sin(xoff),-1,1,0,height)
-        vertex(x,y); 
-        xoff += inc;
+        let y = map(noise(xoff),0,1,0,height);
+        vertex(x,y);
+        xoff += scl;
     }
-    
-    xstart += inc;
 
+    xstart += scl;
     endShape();
-
+    
 }
